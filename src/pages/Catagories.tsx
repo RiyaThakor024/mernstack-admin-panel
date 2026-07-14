01ec43d6ac781d526da30713176016207aaed39f
RiyaThakor024 <riya.thakor024@gmail.com>
2026-07-14 16:27:44 +0530

    react router setup

---
+++ src/pages/Catagories.tsx
@@ -0,0 +1,7 @@
+
+
+export const Catagories = () => {
+  return (
+    <div>Catagories</div>
+  )
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,6 @@
+
+function HomePage() {
+return <h1>Hello ,Welocome to HomePage..!!</h1>;
+}
+
+export default HomePage;
