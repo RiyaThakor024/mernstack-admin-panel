c606f0f8b1765272342adad431b17ce27805046b
RiyaThakor024 <riya.thakor024@gmail.com>
2026-07-14 23:30:39 +0530

    make login pagee

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,19 @@
+import {it , describe, expect} from 'vitest';
+import {render, screen} from '@testing-library/react'
+import { LoginPage } from './login';
+describe('Login Page',()=>{
+    it('should render with required field',()=>{
+           render(<LoginPage />)
+           // getBy -> throws an error
+           // queryBy -> null
+           // findBy -> async
+
+           expect(screen.getByText(/Sign in/)).toBeInTheDocument();
+           expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+           expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+           expect(screen.getByRole('button',{name:'Login'})).toBeInTheDocument();
+           expect(screen.getByRole('checkbox',{name:'Remember me'})).toBeInTheDocument();
+           expect(screen.getByText('Forgot Password')).toBeInTheDocument();
+
+    })
+})
